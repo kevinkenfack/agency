56e8ce51dee9f0de5cc0875e42de738097143a4c
Kevin Kenfack <142149608+kevinkenfack@users.noreply.github.com>
2024-12-05 14:52:29 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import DomainsSection from './components/DomainSection.jsx'
 import AchievementsSection from './components/AchievementsSection.jsx'
 import IdeaSection from './components/IdeaSection.jsx'
 import Footer from './components/Footer.jsx'
+import FloatingComponent from './components/FloatingComponent.jsx'
 
 function App() {
   return (
