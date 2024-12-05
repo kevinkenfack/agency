172e6491502f6c9b219f03d736897063e6a120bd
Kevin Kenfack <kenfackjoelkevin@yahoo.com>
2024-12-05 13:09:09 +0100

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,23 @@
 export default function Header() {
-  return (
-    <header className="relative pt-24 pb-32 px-4 sm:px-6 lg:px-8 overflow-hidden">
-      <div className="max-w-7xl mx-auto">
-        <h1 className="text-6xl sm:text-7xl lg:text-8xl font-bold leading-tight">
-          Your Vision
-          <br />
-          Our{' '}
-          <span className="text-[#5D3B8C]">Expertise</span>
-        </h1>
-      </div>
-
-      {/* Curved Line Image */}
+    return (
+      <header className="w-full min-h-screen  flex flex-col justify-center">
+        <div className="max-w-7xl mx-auto">
+          <h1 className="__className_2eaf5e px-10 py-8 text-5xl md:text-9xl container mx-auto">
+            Your Vision
+            <br />
+            Our{' '}
+            <span className="text-[#5D3B8C]">Expertise</span>
+          </h1>
+        </div>
+        
+        {/* Curved Line SVG */}
       <div className="absolute bottom-0 left-0 w-full">
         <img 
-          src="/hero-line-flow.svg" // Remplace cette URL par celle de ton image
+          src="/hero-line-flow.svg"
           alt="Curved Line"
-          className="w-full h-auto" // Cela rend l'image responsive
+          className="w-full h-auto" 
         />
       </div>
-    </header>
-  );
-}
+      </header>
+    )
+  }  
