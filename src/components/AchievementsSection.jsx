1ff420a6296b7bf0c4eac69159662d9532db73b6
Kevin Kenfack <kenfackjoelkevin@yahoo.com>
2024-12-05 10:11:21 +0100

    update image size

--- src/components/AchievementsSection.jsx
+++ src/components/AchievementsSection.jsx
@@ -11,6 +11,8 @@ export default function AchievementsSection() {
               src="/smart-tab-girl-pink.webp"             
               alt="3D character illustration"
               className="w-full h-auto"
+              width={243}
+              height={500}
             />
           </div>
 
