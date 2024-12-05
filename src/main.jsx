211b979fc353443b0d47a1bd652f2cf0b16eb60d
Kevin Kenfack <kenfackjoelkevin@yahoo.com>
2024-12-05 08:29:35 +0100

    fix rooter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import './index.css'
 
 const root = createRoot(document.getElementById('root'))
 
