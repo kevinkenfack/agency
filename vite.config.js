8bc2246b8bfe99c5bcb2669325ff0cbaeff4ea71
Kevin Kenfack <kenfackjoelkevin@yahoo.com>
2024-12-05 06:31:41 +0100

    add next image depandencies

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['next/image', 'next/link'], // Ajout de next/link
-    },
-  },
-  resolve: {
-    alias: {
-      'next/image': '/path/to/your/custom/ImageComponent.js', // Redirection personnalisée pour next/image si nécessaire
-      'next/link': '/path/to/your/custom/LinkComponent.js',  // Redirection pour next/link
-    },
-  },
-});
+})
