043e60cd1fbb425ff0d17530330ef7b15e7ffd3d
Kevin Kenfack <kenfackjoelkevin@yahoo.com>
2024-12-05 06:25:02 +0100

    fix vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['next/image'], // Ignore le module next/image
+    },
+  },
+  resolve: {
+    alias: {
+      'next/image': '/path/to/your/custom/ImageComponent.js', // Optionnel : redirige vers un composant personnalisé si nécessaire
+    },
+  },
+});
