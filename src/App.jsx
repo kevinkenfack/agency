e476f8b419d5ff6985629df9da9da9d2d176c336
Kevin Kenfack <kenfackjoelkevin@yahoo.com>
2024-12-05 07:25:31 +0100

    resolve header error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './components/Header';
+import Header from './components/Header.jsx';
 import ProductsSection from './components/ProductsSection.jsx'
 import DomainsSection from './components/DomainSection.jsx'
 import AchievementsSection from './components/AchievementsSection.jsx'
