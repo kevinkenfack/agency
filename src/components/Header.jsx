38d1c56eb0e984ef3ee442f66bf0efb459ca0846
Kevin Kenfack <kenfackjoelkevin@yahoo.com>
2024-12-05 13:25:37 +0100

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header className="w-full min-h-screen flex flex-col justify-start items-start relative pb-16">
-      {/* Conteneur du texte positionné en haut à gauche */}
-      <div className="container mx-auto px-10 py-8">
+    <header className="w-full min-h-screen flex flex-col justify-center items-start relative pb-16">
+      {/* Conteneur du texte positionné en bas */}
+      <div className="container mx-auto px-10 py-20 sm:py-24 md:py-28 lg:py-32">
         <h1 className="text-5xl sm:text-6xl md:text-8xl lg:text-9xl font-bold leading-tight">
           Your Vision
           <br />
@@ -12,7 +12,7 @@ export default function Header() {
         </h1>
       </div>
 
-      {/* Curved Line Image */}
+      {/* Image de la ligne courbée */}
       <div className="absolute bottom-0 left-0 w-full">
         <img 
           src="/hero-line-flow.svg" 
