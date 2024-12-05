6ef91835982d64c270382ed0c9e2b88311d94f67
Kevin Kenfack <kenfackjoelkevin@yahoo.com>
2024-12-05 10:50:13 +0100

    update map size

--- src/components/AchievementsSection.jsx
+++ src/components/AchievementsSection.jsx
@@ -54,7 +54,7 @@ export default function AchievementsSection() {
               src="/girl-with-laptop.webp"
 
               alt="3D character with laptop"
-              className="absolute bottom-0 right-0 w-2/2"
+              className="absolute bottom-0 right-0 w-2/4"
             />
           </div>
         </div>
