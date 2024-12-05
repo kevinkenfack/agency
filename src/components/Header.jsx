aef72f9b5d9dde48f8194b4641340d40aac62966
Kevin Kenfack <142149608+kevinkenfack@users.noreply.github.com>
2024-12-05 14:08:18 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,19 +5,19 @@ export default function Header() {
     <header className="w-full min-h-screen flex flex-col justify-center items-center relative pb-16">
       {/* Conteneur du texte centré */}
       <div className="container mx-auto px-6 py-10 sm:py-12 md:py-14 lg:py-16">
-        <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold leading-tight">
+        <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold leading-tight text-center">
           Your Vision
           <br />
           Our <span className="text-[#5D3B8C]">Expertise</span>
         </h1>
       </div>
 
-      {/* Image de la ligne courbée avec transformation sur mobile uniquement */}
+      {/* Image de la ligne courbée */}
       <div className="absolute bottom-0 left-0 w-full">
         <img 
           src="/hero-line-flow.svg" 
           alt="Curved Line"
-          className="w-full h-auto sm:translate-y-[-30rem] md:translate-y-0 lg:translate-y-0"
+          className="w-full h-auto -translate-y-12 sm:-translate-y-10 md:-translate-y-0"
         />
       </div>
     </header>
